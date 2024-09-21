4160c9fc4532c9255a60c6611d609fe843dd91ca
TNTima <87536899+TNTima@users.noreply.github.com>
2024-09-21 16:08:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello from Docker!");
+//Console.ReadKey(); //Couldn't read console from Docker
+Thread.Sleep(60000);
